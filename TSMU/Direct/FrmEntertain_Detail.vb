be78dca2a085418855fc1bbcf89fe9d3d6550bd3
isro <isro@TA-MIS03>
2019-08-27 08:54:12 +0700

    -remark test

--- TSMU/Direct/FrmEntertain_Detail.vb
+++ TSMU/Direct/FrmEntertain_Detail.vb
@@ -84,6 +84,7 @@ Public Class FrmEntertain_Detail
         '' Call Proc_EnableButtons(True, True, True, True, True, True, True, True, True, True)
         Call CreateTable()
         Call InitialSetForm()
+        'remark
 
     End Sub
     Public Overrides Sub InitialSetForm()
