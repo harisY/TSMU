bf7950816cfaa7ffa1a5b350f7fad74af416d865
harisY <haris.yoboseyo@gmail.com>
2020-01-20 08:11:25 +0700

    Update MenuUTama

---
+++ TSMU/Form/PO Check/frm_TAM_InportExcel.vb
@@ -0,0 +1,5 @@
+﻿Public Class frm_TAM_InportExcel
+    Private Sub frm_TAM_InportExcel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call Proc_EnableButtons(False, True, False, True, True, False, False, False, False, False, False)
+    End Sub
+End Class
