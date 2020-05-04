83da6f05afbd960a1e17271e75091b10e33c4c5d
harisY <haris.yoboseyo@gmail.com>
2020-05-04 09:49:05 +0700

    tambah form sales price

--- TSMU/Form/Common/frmSales_ForecastPrice.Designer.vb
+++ TSMU/Form/Common/frmSales_ForecastPrice.Designer.vb
@@ -49,7 +49,7 @@ Partial Class frmSales_ForecastPrice
         Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
         Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExportToExcelTSM, Me.CekHargaADMTSM, Me.CekInventory1ToolStripMenuItem})
         Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
-        Me.ContextMenuStrip1.Size = New System.Drawing.Size(249, 76)
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(249, 104)
         '
         'ExportToExcelTSM
         '
