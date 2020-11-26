f067a75e4af695ecc6c2fb7d7c0d425517d5895d
Miftahgit <55689020+Miftahgit@users.noreply.github.com>
2020-11-26 14:03:43 +0700

    Circulation Report

--- TSMU/My Project/AssemblyInfo.vb
+++ TSMU/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.46")>
-<Assembly: AssemblyFileVersion("1.46")>
+<Assembly: AssemblyVersion("1.48")>
+<Assembly: AssemblyFileVersion("1.48")>
