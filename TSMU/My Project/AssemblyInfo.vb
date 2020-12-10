d3e5d3204e4b25d455baa446e2a69c8b7dc2aa82
erichamedina23 <erichamedina23@gmail.com>
2020-12-10 09:26:09 +0700

    -update version 1.56

--- TSMU/My Project/AssemblyInfo.vb
+++ TSMU/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.53")>
-<Assembly: AssemblyFileVersion("1.53")>
+<Assembly: AssemblyVersion("1.56")>
+<Assembly: AssemblyFileVersion("1.56")>
