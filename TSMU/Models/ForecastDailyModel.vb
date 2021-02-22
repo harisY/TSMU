7ccb268230f0dbb8c75e69bc0e780e719c83d085
harisY <haris.yoboseyo@gmail.com>
2021-02-22 08:33:50 +0700

    Add ForecastDaily Model

---
+++ TSMU/Models/ForecastDailyModel.vb
@@ -0,0 +1,13 @@
+﻿Public Class ForecastDailyModel
+    Public Property Bulan() As Integer
+    Public Property Id() As Integer
+    Public Property PartNo() As String
+    Public Property PONumber() As String
+    Public Property Qty() As Integer
+    Public Property QtyKanban() As Integer
+    Public Property Tahun() As Integer
+    Public Property Tgl() As Integer
+    Public Property UniqueNo() As String
+    Public Property Unit() As String
+    Public Property UploadedDate() As DateTime
+End Class
