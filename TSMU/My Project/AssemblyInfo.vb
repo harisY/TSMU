fb055cc85b5656d0f3bd9249f3bd3eb33e86a5d0
erichamedina23 <erichamedina23@gmail.com>
2021-03-02 11:19:42 +0700

    -update version 1.90 -edit upload non idr to solomon

--- TSMU/My Project/AssemblyInfo.vb
+++ TSMU/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.89")>
-<Assembly: AssemblyFileVersion("1.89")>
+<Assembly: AssemblyVersion("1.90")>
+<Assembly: AssemblyFileVersion("1.90")>
