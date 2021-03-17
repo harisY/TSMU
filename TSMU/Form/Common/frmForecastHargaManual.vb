673a38e7d4e047c05394f72149a43e542c1ce702
harisY <haris.yoboseyo@gmail.com>
2021-03-18 00:12:51 +0700

    add Form Forecast Harga Manual

---
+++ TSMU/Form/Common/frmForecastHargaManual.vb
@@ -0,0 +1,86 @@
+﻿Public Class frmForecastHargaManual
+    Dim Service As New TForecastPrice_TempService
+    Dim Model As TForecastPrice_TempModel
+    Dim ff_Detail As FrmForecastPriceTemp
+
+    Private Sub frmForecastHargaManual_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call Proc_EnableButtons(True, False, True, True, True, False, False, False, False, False, False, False)
+        LoadGrid()
+    End Sub
+
+    Private Sub LoadGrid()
+        Try
+            Dim dt As New DataTable
+            dt = Service.GetDataGrid()
+
+            Grid.DataSource = dt
+            With GridView1
+                .BestFitColumns()
+            End With
+            If GridView1.RowCount > 0 Then
+                GridCellFormat(GridView1)
+            End If
+        Catch ex As Exception
+            ShowMessage(ex.Message, MessageTypeEnum.ErrorMessage)
+            WriteToErrorLog(ex.Message, gh_Common.Username, ex.StackTrace)
+        End Try
+    End Sub
+
+    Public Overrides Sub Proc_Refresh()
+        Call LoadGrid()
+    End Sub
+
+    Public Overrides Sub Proc_InputNewData()
+        'GridView1.AddNewRow()
+
+        CallFrm()
+    End Sub
+
+    Private Sub CallFrm(Optional ByVal ls_Code As Integer = 0, Optional ByVal ls_Code2 As String = "", Optional ByVal li_Row As Integer = 0)
+        If ff_Detail IsNot Nothing AndAlso ff_Detail.Visible Then
+            If MsgBox(gs_ConfirmDetailOpen, MsgBoxStyle.OkCancel, "Confirmation") = MsgBoxResult.Cancel Then
+                Exit Sub
+            End If
+            ff_Detail.Close()
+        End If
+        ff_Detail = New FrmForecastPriceTemp(ls_Code, ls_Code2, Me, li_Row, Grid)
+        'ff_Detail.MdiParent = FrmMain
+        ff_Detail.StartPosition = FormStartPosition.CenterScreen
+        ff_Detail.ShowDialog()
+    End Sub
+
+    Private Sub PassDataToDetail()
+        Dim Id As Integer
+        Try
+            Id = 0
+            Dim selectedRows() As Integer = GridView1.GetSelectedRows()
+            For Each rowHandle As Integer In selectedRows
+                If rowHandle >= 0 Then
+                    Id = GridView1.GetRowCellValue(rowHandle, "Id")
+                End If
+            Next rowHandle
+            Dim n As Integer = 0
+            If GridView1.GetSelectedRows.Length > 0 Then
+                For i As Integer = 0 To GridView1.FocusedRowHandle - 1
+                    n += 1
+                Next
+                'Dim objGrid As DataGridView = sender
+                Call CallFrm(Id, n)
+            End If
+        Catch ex As Exception
+            ShowMessage(ex.Message, MessageTypeEnum.ErrorMessage)
+            WriteToErrorLog(ex.Message, gh_Common.Username, ex.StackTrace)
+        End Try
+    End Sub
+
+    Private Sub frmForecastHargaManual_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.F1 Then
+            PassDataToDetail()
+        End If
+    End Sub
+
+    Private Sub Grid_DoubleClick(sender As Object, e As EventArgs) Handles Grid.DoubleClick
+        PassDataToDetail()
+    End Sub
+
+End Class
--- TSMU/Services/TForecastPrice_TempService.vb
+++ TSMU/Services/TForecastPrice_TempService.vb
@@ -1,10 +1,42 @@
 ﻿Imports System.Data.SqlClient
 
 Public Class TForecastPrice_TempService
-
-    Public Sub InsertTemptData(Model As TForecastPrice_TempModel)
+    Dim Model As TForecastPrice_TempModel
+    Public Function GetDataGrid() As DataTable
+        Try
+            Dim dt As New DataTable
+            dt = GetDataTableByParam("tForecastPrice_Temp1Select", CommandType.StoredProcedure, Nothing, GetConnString)
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+    Public Sub GetDataById(Id As Integer)
         Try
             Dim parameters As List(Of SqlParameter) = New List(Of SqlParameter)()
+            parameters.Add(New SqlParameter() With {.ParameterName = "Id", .Value = Id})
+            Dim dt As New DataTable
+            dt = GetDataTableByParam("tForecastPrice_Temp1SelectById", CommandType.StoredProcedure, Nothing, GetConnString)
+            If dt.Rows.Count > 0 Then
+                With Model
+                    .Id = dt.Rows(0)("Id").ToString().AsString()
+                    .Tahun = dt.Rows(0)("Tahun").ToString().AsString()
+                    .Bulan = dt.Rows(0)("Bulan").ToString().AsString()
+                    .CustID = dt.Rows(0)("CustID").ToString().AsString()
+                    .InvtID = dt.Rows(0)("InvtID").ToString().AsString()
+                    .PartNo = dt.Rows(0)("PartNo").ToString().AsString()
+                    .PartName = dt.Rows(0)("PartName").ToString().AsString()
+                    .Site = dt.Rows(0)("Site").ToString().AsString()
+                    .Harga = dt.Rows(0)("Harga").ToString().AsDouble()
+                End With
+            End If
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+    Public Sub InsertData(Model As TForecastPrice_TempModel)
+        Try
+            Dim parameters As List(Of SqlParameter) = New List(Of SqlParameter)()
+            parameters.Add(New SqlParameter() With {.ParameterName = "Id", .Value = Model.Id})
             parameters.Add(New SqlParameter() With {.ParameterName = "Tahun", .Value = Model.Tahun})
             parameters.Add(New SqlParameter() With {.ParameterName = "Bulan", .Value = Model.Bulan})
             parameters.Add(New SqlParameter() With {.ParameterName = "CustID", .Value = Model.CustID})
