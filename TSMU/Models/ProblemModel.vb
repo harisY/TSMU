48a1af0ae694f26429f12e561e4e460723e70876
harisY <haris.yoboseyo@gmail.com>
2021-03-30 14:26:58 +0700

    Add Problem Class

---
+++ TSMU/Models/ProblemModel.vb
@@ -0,0 +1,6 @@
+﻿Public Class ProblemModel
+    Public Property CreatedDate() As Date
+    Public Property Id() As Integer
+    Public Property Name() As String
+    Public Property ProblemCode() As String
+End Class
