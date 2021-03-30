15c0e853434ba4d4faccad0b55e96e281ef001da
harisY <haris.yoboseyo@gmail.com>
2021-03-30 10:22:39 +0700

    add Form monitoring inventory

---
+++ TSMU/Form/Common/frmMonitoringInventory.vb
@@ -0,0 +1,59 @@
+﻿Imports DevExpress.LookAndFeel
+Imports DevExpress.XtraEditors
+Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.XtraPrinting
+Imports DevExpress.XtraReports.UI
+Imports DevExpress.XtraSplashScreen
+
+Public Class frmMonitoringInventory
+    Dim dtGrid As DataTable
+    Dim Service As New SolomonService
+
+    Private Sub frmMonitoringInventory_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        bb_SetDisplayChangeConfirmation = False
+        Call Proc_EnableButtons(False, False, False, True, True, False, True, False, False, False, False)
+    End Sub
+
+    Private Sub LoadGrid()
+        Try
+            SplashScreenManager.ShowForm(GetType(FrmWait))
+            dtGrid = New DataTable
+            dtGrid = Service.GetDataInventory(Date.Today, Date.Today)
+            Grid.DataSource = dtGrid
+
+            With GridView1
+                .BestFitColumns()
+            End With
+            If GridView1.RowCount > 0 Then
+                GridCellFormat(GridView1)
+            End If
+            SplashScreenManager.CloseForm()
+        Catch ex As Exception
+            SplashScreenManager.CloseForm()
+            Call ShowMessage(ex.Message, MessageTypeEnum.ErrorMessage)
+            WriteToErrorLog(ex.Message, gh_Common.Username, ex.StackTrace)
+        End Try
+    End Sub
+    Public Overrides Sub Proc_Refresh()
+        LoadGrid()
+    End Sub
+    Public Overrides Sub Proc_Excel()
+        If GridView1.RowCount > 0 Then
+            Dim save As New SaveFileDialog With {
+                .Filter = "Excel File|*.xlsx",
+                .Title = "Save an Excel File"
+            }
+            If save.ShowDialog = DialogResult.OK Then
+                _Grid.ExportToXlsx(save.FileName)
+            End If
+        Else
+            ShowMessage("Data Kosong !", MessageTypeEnum.ErrorMessage)
+        End If
+    End Sub
+
+
+    Private Sub frmMonitoringInventory_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        LoadGrid()
+    End Sub
+
+End Class
---
+++ TSMU/Services/SolomonService.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Data.SqlClient
+
+Public Class SolomonService
+    Public Function GetDataInventory(FromDate As Date, ToDate As Date) As DataTable
+        Try
+            Dim Query As String =
+            "
+            SELECT
+	            Crtd_User,
+	            Crtd_DateTime,
+	            InvtID,
+	            Descr,
+	            StkUnit,
+	            CASE valmthd
+		            WHEN 'F' THEN 'FIFO'
+		            WHEN 'L' THEN 'LIFO'
+		            WHEN 'A' THEN 'Average Cost'
+		            WHEN 'S' THEN 'Specific Identification'
+		            WHEN 'T' THEN 'Standard Cost'
+		            WHEN 'U' THEN 'User-Specified Cost'
+		            ELSE ''
+	            END AS Methode,
+	            InvtAcct,
+	            COGSAcct,
+	            DfltSalesAcct,
+	            ppvacct,
+	            CASE TranStatusCode
+		            WHEN 'AC' THEN 'Active'
+		            WHEN 'NP' THEN 'No Purchase'
+		            WHEN 'NU' THEN 'No Usage'
+		            WHEN 'OH' THEN 'On Hold'
+		            WHEN 'IN' THEN 'Inactive'
+		            WHEN 'DE' THEN 'Delete'
+		            ELSE ''
+	            END AS STATUS
+            FROM Inventory
+            WHERE Crtd_User <> 'sysadmin' and Convert(Date,crtd_datetime) >=@FromDate
+            AND Convert(Date,crtd_datetime) <= @ToDate      
+            ORDER BY crtd_datetime
+            "
+            Dim Params As List(Of SqlParameter) = New List(Of SqlParameter) From {
+                New SqlParameter() With {.ParameterName = "FromDate", .Value = FromDate},
+                New SqlParameter() With {.ParameterName = "ToDate", .Value = ToDate}
+                }
+            Dim dt As New DataTable
+            dt = GetDataTableByParam(Query, CommandType.Text, Params, GetConnStringSolomon)
+            Return dt
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+
+End Class
